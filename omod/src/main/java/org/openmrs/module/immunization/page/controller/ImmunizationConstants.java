20954105687cdfcc25376125d1378d064fb7b897
injiri <simoninjiri@gmail.com>
2020-10-27 14:19:14 +0300

    code setup for fire actions from kenyaemr home >config overhall

---
+++ omod/src/main/java/org/openmrs/module/immunization/page/controller/ImmunizationConstants.java
@@ -0,0 +1,7 @@
+package org.openmrs.module.immunization.page.controller;
+
+public class ImmunizationConstants {
+
+  public static final String MODULE_ID = "patientImmunization";
+  public static final String APP_IMMUNIZATION = MODULE_ID + ".immunization";
+}
---
+++ omod/src/main/java/org/openmrs/module/immunization/page/controller/PatientImmunizationHomePageController.java
@@ -0,0 +1,11 @@
+package org.openmrs.module.immunization.page.controller;
+
+import org.openmrs.module.kenyaui.annotation.AppPage;
+import org.openmrs.ui.framework.UiUtils;
+import org.openmrs.ui.framework.page.PageModel;
+
+@AppPage(ImmunizationConstants.APP_IMMUNIZATION)
+public class PatientImmunizationHomePageController {
+
+  public void controller(UiUtils ui, PageModel model) {}
+}
